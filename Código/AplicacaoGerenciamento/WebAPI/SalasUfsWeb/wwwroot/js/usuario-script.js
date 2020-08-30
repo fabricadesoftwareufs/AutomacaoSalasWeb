f376f2085eeeed6f5d50eb7ab522c79528b71466
abralvs <abraao1514@gmail.com>
2020-08-29 23:13:02 -0300

    Update usuario-script.js

--- Código/AplicacaoGerenciamento/WebAPI/SalasUfsWeb/wwwroot/js/usuario-script.js
+++ Código/AplicacaoGerenciamento/WebAPI/SalasUfsWeb/wwwroot/js/usuario-script.js
@@ -5,12 +5,10 @@
             if (data) {
                 $('.obs').text('O usuário contem planejamento ou reservas associadas.');
                 $('.obs').addClass('text-danger');
-                console.log("ssasasas");
             }
             else {
                 $('.obs').text('O usuário não contém nenhuma observação.');
                 $('.obs').removeClass('text-danger');
-                console.log("aaaaaaaaaaa");
             }
         });
         
