e968568fa2a506e7134945a19620da0411a237d6
abralvs <abraao1514@gmail.com>
2020-12-09 20:24:10 -0300

    corrigindo substituicoes do metodo

--- Código/AplicacaoGerenciamento/WebAPI/Business/MonitoramentoService.cs
+++ Código/AplicacaoGerenciamento/WebAPI/Business/MonitoramentoService.cs
@@ -106,7 +106,7 @@ namespace Service
 
                 var mensagem = MontarMensagemComComandosIr("condicionador;", codigosInfravermelho);
                 var clienteSocket = new ClienteSocketService(hardwareDeSala.Ip);
-                comandoEnviadoComSucesso = clienteSocket.EnviarComando(mensagem);
+                comandoEnviadoComSucesso = clienteSocket.EnviarComando(mensagem) != null ? true : false;
             }
             else if (solicitacao.Luzes != modelDesatualizado.Luzes)
             {
@@ -121,7 +121,7 @@ namespace Service
                 var mensagem = MontarMensagemComComandosIr("luzes;", codigosInfravermelho);
 
                 var clienteSocket = new ClienteSocketService(hardwareDeSala.Ip);
-                comandoEnviadoComSucesso = clienteSocket.EnviarComando(mensagem);
+                comandoEnviadoComSucesso = clienteSocket.EnviarComando(mensagem) != null ? true : false;
             }
 
             return comandoEnviadoComSucesso;
