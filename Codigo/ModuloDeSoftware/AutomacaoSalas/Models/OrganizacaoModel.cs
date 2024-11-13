64051425209c04fa043eab0fb91d86e93a22b0e2
miguelftt <miguelferreirafranca3030@gmail.com>
2024-11-13 16:07:48 -0300

    close #157

--- Codigo/ModuloDeSoftware/AutomacaoSalas/Models/OrganizacaoModel.cs
+++ Codigo/ModuloDeSoftware/AutomacaoSalas/Models/OrganizacaoModel.cs
@@ -7,10 +7,12 @@ namespace Model
         [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Código")]
         public uint Id { get; set; }
+       
         [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "CNPJ")]
-        [StringLength(14, ErrorMessage = "Máximo são 14 caracteres")]
+        [StringLength(18, ErrorMessage = "Máximo são 14 caracteres")]
         public string Cnpj { get; set; }
+        
         [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Razão Social")]
         [StringLength(45, ErrorMessage = "Máximo são 45 caracteres")]
