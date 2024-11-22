0027c9045543b331c6b5d3a4b84b4cc0c12fb73e
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-11-22 10:57:50 -0300

    add função JS

---
+++ Codigo/ModuloDeSoftware/AutomacaoSalas/SalasWeb/wwwroot/js/formatData.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.data-format').forEach(element => {
+        const dataISO = element.textContent.trim(); 
+        const dataBR = new Date(dataISO).toLocaleDateString('pt-BR'); 
+        element.textContent = dataBR; 
+    });
+});
+
