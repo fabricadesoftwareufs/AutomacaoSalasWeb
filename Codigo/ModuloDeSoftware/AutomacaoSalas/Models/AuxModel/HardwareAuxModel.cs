03706769aef707cd72be4da866c30a05a551105b
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-12-27 10:51:05 -0300

    Problema resolvido!

--- Codigo/ModuloDeSoftware/AutomacaoSalas/Models/AuxModel/HardwareAuxModel.cs
+++ Codigo/ModuloDeSoftware/AutomacaoSalas/Models/AuxModel/HardwareAuxModel.cs
@@ -3,7 +3,7 @@
     public class HardwareAuxModel
     {
         public string MAC { get; set; }
-        public string Ip { get; set; }
+        public string? Ip { get; set; }
         public TipoUsuarioModel TipoHardwareId { get; set; }
     }
 }
